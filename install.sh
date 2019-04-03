6757b63ea3eed84fea68d135d68f525df14a2eef
Fomenko Olga <o.fomenko@cardnonstop.com>
2019-04-03 10:36:55 +0400

    create install.sh

---
+++ install.sh
@@ -0,0 +1,29 @@
+# create a .env file
+cp .env.example .env
+
+# install composer dependencies
+composer install
+
+# install npm dependencies
+npm install
+
+# generate a key for your application
+php artisan key:generate
+
+# create a local MySQL database (make sure you have MySQL up and running)
+mysql -u root
+
+> create database vue;
+> exit;
+
+# add the database connection config to your .env file
+DB_CONNECTION=mysql
+DB_DATABASE=vue
+DB_USERNAME=root
+DB_PASSWORD=
+
+# run the migration files to generate the schema
+php artisan migrate
+
+# run webpack and watch for changes
+npm run watch
